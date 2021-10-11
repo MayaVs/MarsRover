f6bd6a9c97d2c145565f6777ca71338e8caca492
Maya Vasileva <maya.vassileva@gmail.com>
2021-10-11 02:07:45 +0100

    Added test with rovers landing on the same position and it trows exception

--- src/test/java/com/techreturn/test/MarsRoversTest.java
+++ src/test/java/com/techreturn/test/MarsRoversTest.java
@@ -1,12 +1,17 @@
 package com.techreturn.test;
 
 import com.techreturn.mars.*;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MarsRoversTest {
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
 
     @Test
     public void MultipleRoverTest() throws Exception {
@@ -18,6 +23,25 @@ public class MarsRoversTest {
         roversList.add(rover2);
 
         MarsRovers marsRovers = new MarsRovers(roversList, plateau);
+
+    }
+
+    @Test
+    public void MultipleRoverCrushTest() throws Exception {
+        exceptionRule.expect(Exception.class);
+
+
+        Plateau plateau = new Plateau(5, 5);
+        ArrayList<MarsVehicles> roversList = new ArrayList<>();
+        Rover rover1 = new Rover(0, 0, Orientation.W, plateau);
+        Rover rover2 = new Rover(0, 0, Orientation.N, plateau);
+        roversList.add(rover1);
+        roversList.add(rover2);
+
+        MarsRovers marsRovers = new MarsRovers(roversList, plateau);
+
+        //Assert
+        exceptionRule.expectMessage("Not valid position for Rover");
 
     }
 }
